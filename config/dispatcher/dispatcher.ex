b6250eaa77983c71449c0d3242a05d2de208b465
Erika Pauwels <erika.pauwels@gmail.com>
2026-03-30 13:21:34 +0200

    Replace cadvisor with docker-prom-metrics

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -7,25 +7,8 @@ defmodule Dispatcher do
   end
 
   get "/container-metrics", _ do
-    forward conn, [], "http://cadvisor:8080/metrics"
+    forward conn, [], "http://docker-metrics/metrics"
   end
-
-  ## Rules below are for the cadvisor inteface which we do not make public by default
-  # get "/static/*path", _ do
-  #   forward conn, path, "http://cadvisor:8080/static/"
-  # end
-
-  # match "/api/*path", _ do
-  #   forward conn, path, "http://cadvisor:8080/api/"
-  # end
-
-  # get "/containers/*path", _ do
-  #   forward conn, path, "http://cadvisor:8080/containers/"
-  # end
-
-  # get "/docker/*path", _ do
-  #   forward conn, path, "http://cadvisor:8080/docker/"
-  # end
 
   match "/*_", %{ last_call: true } do
     send_resp( conn, 404, "Route not found or method not supported" )
